837b92c1b6a06fd3b17152ffc1c83eeb15af7697
Pedro-Louzada <pelulouzada@gmail.com>
2024-09-05 22:51:45 -0300

    utilizating react-router-dom and creating layout for our routes dynamiclly'

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from "styled-components";
 
-import { Button } from "./components/Button";
-import { defaulTheme } from "./styles/themes/default";
+import { BrowserRouter } from "react-router-dom";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+
 
 export function App() {
   return (
-    <ThemeProvider theme={defaulTheme}>
-      <Button variant='primary'/>
-      <Button variant='secundary'/>
-      <Button variant='success'/>
-      <Button variant='danger'/>
-      <Button />
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 };
